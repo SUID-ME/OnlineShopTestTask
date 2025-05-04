6ff96350cf6f769911d56a9f2bf1343feb98dba7
SUID-ME <86769839+SUID-ME@users.noreply.github.com>
2025-05-04 10:16:08 +0300

    Add DataGrid

--- PulsarTestTask/ViewModels/PageShoppingCartVM.cs
+++ PulsarTestTask/ViewModels/PageShoppingCartVM.cs
@@ -1,6 +1,22 @@
-﻿namespace PulsarTestTask.ViewModels
+﻿using DynamicData;
+using PulsarTestTask.Models;
+using ReactiveUI.Fody.Helpers;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace PulsarTestTask.ViewModels
 {
     public class PageShoppingCartVM : ViewModelBase
     {
+        public ObservableCollection<CartContent> CartContent { get; set; } = App.CartList;
+    }
+
+    public class CartContent : ViewModelBase
+    {
+        public required string Name { get; set; }
+        [Reactive]
+        public int Count { get; set; }
     }
 }

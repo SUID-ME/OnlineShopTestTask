e0f884597e0094ee81602ee114912d5e407aae69
SUID-ME <86769839+SUID-ME@users.noreply.github.com>
2025-05-04 16:01:44 +0300

    Add Converters

---
+++ PulsarTestTask/Converters/BoolToBrushConverter.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Globalization;
+
+namespace PulsarTestTask.Converters
+{
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public static readonly BoolToBrushConverter Instance = new();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isAvailable)
+            {
+                return isAvailable
+                    ? Brushes.Green
+                    : Brushes.Red;
+            }
+            return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ PulsarTestTask/Converters/BoolToGeometryConverter.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Globalization;
+
+namespace PulsarTestTask.Converters
+{
+    public class BoolToGeometryConverter : IValueConverter
+    {
+        public static readonly BoolToGeometryConverter Instance = new();
+
+        const string checkMark = "M4,12 L9,17 L20,6";
+        const string crossMark = "M 5,5 L 19,19 M 5,19 L 19,5";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isAvailable)
+            {
+                return Geometry.Parse(isAvailable ? checkMark : crossMark);
+            }
+            return Geometry.Parse("");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}

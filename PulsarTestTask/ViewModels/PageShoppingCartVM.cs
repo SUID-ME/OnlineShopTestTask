e155fd79e6df3ff1f77f84dc1623502ba9672de8
SUID-ME <86769839+SUID-ME@users.noreply.github.com>
2025-05-04 11:28:23 +0300

    Improve DataGrid

--- PulsarTestTask/ViewModels/PageShoppingCartVM.cs
+++ PulsarTestTask/ViewModels/PageShoppingCartVM.cs
@@ -1,9 +1,30 @@
-﻿using System.Collections.ObjectModel;
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.ObjectModel;
 
 namespace PulsarTestTask.ViewModels
 {
     public class PageShoppingCartVM : ViewModelBase
     {
-        public ObservableCollection<CartContent> CartContent { get; set; } = App.CartList;
+        #region Constructor
+        public PageShoppingCartVM() {
+            CartContent = App.CartList;
+            CartListCount = CartContent.Count;
+
+            this.WhenAnyValue(x => x.CartContent.Count)
+                .Subscribe(count =>
+                {
+                    CartListCount = count;
+                });
+        }
+        #endregion Constructor
+
+        #region Bindings
+        public ObservableCollection<CartContent> CartContent { get; }
+
+        [Reactive]
+        public int CartListCount { get; set; }
+        #endregion Bindings
     }
 }

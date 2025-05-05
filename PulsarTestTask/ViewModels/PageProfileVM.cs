919428b3b0d6d9e1f550d24b48615acadd4aa3d8
SUID-ME <86769839+SUID-ME@users.noreply.github.com>
2025-05-05 16:33:32 +0300

    Display avatar and base user info

--- PulsarTestTask/ViewModels/PageProfileVM.cs
+++ PulsarTestTask/ViewModels/PageProfileVM.cs
@@ -1,8 +1,22 @@
-﻿using ReactiveUI.Fody.Helpers;
+﻿using Avalonia.Media.Imaging;
+using PulsarTestTask.Converters;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace PulsarTestTask.ViewModels
 {
     public class PageProfileVM : ViewModelBase
+    {
+        public PageProfileVM()
+        {
+            _bitmapLoader.LoadBitmap(UserInfo);
+        }
+
+        public UserInfo UserInfo { get; set; } = new();
+        private BitmapLoader<UserInfo> _bitmapLoader = new();
+    }
+
+    public class UserInfo() : ReactiveObject, IUrl
     {
         public ProfileAttributeVM UserFirstName { get; set; } = new ProfileAttributeVM("Имя", "Иван");
         public ProfileAttributeVM UserSecondName { get; set; } = new ProfileAttributeVM("Фамилия", "Иванов");
@@ -13,5 +27,9 @@ namespace PulsarTestTask.ViewModels
 
         public ProfileAttributeVM UserAge { get; set; } = new ProfileAttributeVM("Возраст", "28 лет");
         public ProfileAttributeVM UserBalance { get; set; } = new ProfileAttributeVM("Баланс", "4000р");
+
+        [Reactive]
+        public Bitmap? Bitmap { get; set; }
+        public string Url { get; set; } = "https://i.pinimg.com/736x/2f/90/c1/2f90c1e9fc45169c0f2cc0dfe88e73a5.jpg";
     }
 }

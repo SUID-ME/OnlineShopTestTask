199c6646ec6cad4582fc21bc9742f08255384e71
SUID-ME <86769839+SUID-ME@users.noreply.github.com>
2025-05-06 15:34:39 +0300

    Remove context menu from textbox

---
+++ PulsarTestTask/Behaviors/RightClickBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia.Controls;
+using Avalonia;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+using System.Reactive.Linq;
+using System.Windows.Input;
+
+namespace PulsarTestTask.Behaviors;
+
+public class RightClickBehavior : Behavior<Control>
+{
+    public static readonly AvaloniaProperty<ICommand> CommandProperty =
+        AvaloniaProperty.Register<RightClickBehavior, ICommand>(nameof(Command));
+
+    public ICommand Command
+    {
+        get => (ICommand)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+    protected override void OnAttached()
+    {
+        AssociatedObject?.AddHandler(
+            InputElement.PointerPressedEvent,
+            OnPointerPressed,
+            RoutingStrategies.Tunnel);
+    }
+
+    private void OnPointerPressed(object sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(AssociatedObject).Properties.IsRightButtonPressed)
+        {
+            Command?.Execute(null);
+        }
+    }
+}
